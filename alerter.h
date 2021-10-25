bb2ab4b4268908c3f4aed340c2af320996f992fe
Harshini Narasimha <91076579+Harshini-Narasimha@users.noreply.github.com>
2021-10-25 18:47:27 +0530

    Create alerter.h

---
+++ alerter.h
@@ -0,0 +1,57 @@
+class IAlerter
+{
+  public:
+      virtual void sendAlert(bool alertStatus)=0;
+};
+
+class EmailAlert: public IAlerter
+{
+ public:
+    EmailAlert(): emailSent(false) {}
+
+    void sendAlert(bool alertStatus)
+    {
+      emailSent=alertStatus;
+    }
+    
+    //member variable
+    bool emailSent;
+};
+
+class LEDAlert: public IAlerter
+{
+ public:
+    LEDAlert(): ledGlows(false) {}
+    
+    void sendAlert(bool alertStatus)
+    {
+      ledGlows=alertStatus;
+    }
+
+    //member variable
+    bool ledGlows;
+};
+
+class StatsAlerter
+{
+ public:
+   StatsAlerter():maxThreshold(0.0){}
+   StatsAlerter(float p_maxThreshold,std::vector<IAlerter*> alerterList):maxThreshold(p_maxThreshold),alerters(alerterList){}
+   
+   void checkAndAlert(const std::vector<float>& sensorValues)
+   {
+      Statistics::Stats statsResult = Statistics::ComputeStatistics(sensorValues);
+      if(statsResult.max > maxThreshold)
+      {
+          alerters[0]->sendAlert(true);
+          alerters[1]->sendAlert(true);
+          /*std::vector<IAlerter*>::iterator itAlerts;
+          for (itAlerts = alerters.begin(); itAlerts != alerters.end(); itAlerts++)
+          {
+	      itAlerts->sendAlert(true);  
+          }*/
+      }
+   }
+   float maxThreshold;
+   std::vector<IAlerter*> alerters;
+};
